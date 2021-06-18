d1cf151c2f12b14b97f243e29b71492f15bfec2f
Christian Scheel <christian.scheel@bluewin.ch>
2021-06-18 15:12:03 +0200

    First commit

---
+++ SOS-Beep/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SOS_Beep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Implement infinite loop until user presses a button
+            for (int i = 0; i <3; i++)
+            {
+                Console.Beep(900 ,150);
+            }
+            Console.Beep(37, 150);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Beep(900, 400);
+            }
+            Console.Beep(37, 150);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Beep(900, 150);
+            }
+        }
+    }
+}
