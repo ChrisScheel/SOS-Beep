e4df7477d7a8928520aada6da9a65537c3e9f416
Christian Scheel <christian.scheel@bluewin.ch>
2021-06-20 14:48:55 +0200

    Modify program to print out symbols too

--- SOS-Beep/Program.cs
+++ SOS-Beep/Program.cs
@@ -1,25 +1,39 @@
 ﻿using System;
+using System.Threading;
 
 namespace SOS_Beep
 {
     class Program
     {
+        //Program works only with Windows operating systems
+        //TODO: Implement KeyListener to exit program after user pressed Enter
         static void Main(string[] args)
         {
-            //TODO: Implement infinite loop until user presses a button
-            for (int i = 0; i <3; i++)
+            int frequencyInHertz = 400;
+            int shortBeepInMilliseconds = 220;
+            int longBeepInMilliseconds = 525;
+            string shortBeepSymbol = ". ";
+            string longBeepSymbol = "- ";
+
+            while (true)
             {
-                Console.Beep(900 ,150);
-            }
-            Console.Beep(37, 150);
-            for (int i = 0; i < 3; i++)
-            {
-                Console.Beep(900, 400);
-            }
-            Console.Beep(37, 150);
-            for (int i = 0; i < 3; i++)
-            {
-                Console.Beep(900, 150);
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Beep(frequencyInHertz, shortBeepInMilliseconds);
+                    Console.Write($"{shortBeepSymbol}");
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Beep(frequencyInHertz, longBeepInMilliseconds);
+                    Console.Write($"{longBeepSymbol}");
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Beep(frequencyInHertz, shortBeepInMilliseconds);
+                    Console.Write($"{shortBeepSymbol}");
+                }
+                Console.WriteLine();
+                Thread.Sleep(500);
             }
         }
     }
