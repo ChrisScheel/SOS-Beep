21a25780ad323a77cf9d72cd3f71219f8397ee82
Christian Scheel <christian.scheel@bluewin.ch>
2021-06-21 16:24:32 +0200

    Add function to abort programm after the user pressed Enter

--- SOS-Beep/Program.cs
+++ SOS-Beep/Program.cs
@@ -7,26 +7,34 @@ namespace SOS_Beep
     {
         //Program works only with Windows operating systems
         //TODO:
-        //- Implement KeyListener to exit program after user pressed Enter
         //- Add Exception handling if number is <37 or bigger then 32767
         static void Main(string[] args)
         {
             Console.WriteLine("Please type in a number for the frequency in Hertz.\n" +
                 "The number must be between 37 and 32767");
             int frequencyInHertz = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n Press Enter to stopp the program after an SOS iteration");
             SignalSOS(frequencyInHertz);
         }
 
         static void SignalSOS(int frequency)
         {
             int frequencyInHertz = frequency;
-            const int shortBeepInMilliseconds = 220;
-            const int longBeepInMilliseconds = 525;
-            const string shortBeepSymbol = ". ";
-            const string longBeepSymbol = "- ";
+            int shortBeepInMilliseconds = 220;
+            int longBeepInMilliseconds = 525;
+            string shortBeepSymbol = ". ";
+            string longBeepSymbol = "- ";
 
-            while (true)
+            do
             {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                }
                 for (int i = 0; i < 3; i++)
                 {
                     Console.Beep(frequencyInHertz, shortBeepInMilliseconds);
@@ -44,7 +52,8 @@ namespace SOS_Beep
                 }
                 Console.WriteLine();
                 Thread.Sleep(500);
-            }
+
+            } while (true);
         }
     }
 }
