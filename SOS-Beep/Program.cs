2b1e3fb683cda6a7968d45f0f1617d91af9a1efd
Christian Scheel <christian.scheel@bluewin.ch>
2021-06-28 11:12:41 +0200

    Refactor Program class for better readability
    Static method SignalSOS deleted and included in new class
    SOSBeeperGenerator.

--- SOS-Beep/Program.cs
+++ SOS-Beep/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 
 namespace SOS_Beep
 {
@@ -9,9 +8,7 @@ namespace SOS_Beep
         //TODO:
         //- Modify exception handling so the user can enter a value again
         //- Add maybe something to run beeping and text output parallel
-        //- Refactor code because it became too big:
         //  - Handle exception handling elsewhere
-        //  - Split the SignalSOS() method to make it more readable
         static void Main(string[] args)
         {
             Console.WriteLine("Please type in a number for the frequency in Hertz.\n" +
@@ -20,7 +17,8 @@ namespace SOS_Beep
             {
                 int frequencyInHertz = int.Parse(Console.ReadLine());
                 Console.WriteLine("\n Press Enter to stopp the program after an SOS iteration");
-                SignalSOS(frequencyInHertz);
+                SOSBeeperGenerator sOSBeeper = new SOSBeeperGenerator(frequencyInHertz);
+                sOSBeeper.SignalSOS();
             }
             catch (System.FormatException)
             {
@@ -32,47 +30,6 @@ namespace SOS_Beep
                 Console.ForegroundColor = System.ConsoleColor.Red;
                 Console.WriteLine("\n The number must be between 37 und 32767!");
             }
-        }
-
-        static void SignalSOS(int frequency)
-        {
-            int frequencyInHertz = frequency;
-            int shortBeepInMilliseconds = 220;
-            int longBeepInMilliseconds = 525;
-            string shortBeepSymbol = ". ";
-            string longBeepSymbol = "- ";
-
-            do
-            {
-                //Info for me: Console.ReadKey() waits for user input. The program stops till then.
-                //With Console.KeyAvailable() that's not the case
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    if (key.Key == ConsoleKey.Enter)
-                    {
-                        Console.WriteLine("SOS stopped");
-                        break;
-                    }
-                }
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.Beep(frequencyInHertz, shortBeepInMilliseconds);
-                    Console.Write($"{shortBeepSymbol}");
-                }
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.Beep(frequencyInHertz, longBeepInMilliseconds);
-                    Console.Write($"{longBeepSymbol}");
-                }
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.Beep(frequencyInHertz, shortBeepInMilliseconds);
-                    Console.Write($"{shortBeepSymbol}");
-                }
-                Console.WriteLine();
-                Thread.Sleep(500);
-            } while (true);
         }
     }
 }
---
+++ SOS-Beep/SOSBeeperGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace SOS_Beep
+{
+    class SOSBeeperGenerator
+    {
+        private int frequencyInHertz = 0;
+        private readonly int shortBeepInMilliseconds = 220;
+        private readonly int longBeepInMilliseconds = 525;
+        private readonly string shortBeepSymbol = ". ";
+        private readonly string longBeepSymbol = "- ";
+
+        public SOSBeeperGenerator(int frequencyInHertz)
+        {
+            this.frequencyInHertz = frequencyInHertz;
+        }
+
+        public void SignalSOS()
+        {
+            do
+            {
+                //Info for me: Console.ReadKey() waits for user input. The program stops till then.
+                //With Console.KeyAvailable() that's not the case
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                        Console.WriteLine("SOS has stopped");
+                        break;
+                    }
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Beep(frequencyInHertz, shortBeepInMilliseconds);
+                    Console.Write($"{shortBeepSymbol}");
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Beep(frequencyInHertz, longBeepInMilliseconds);
+                    Console.Write($"{longBeepSymbol}");
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Beep(frequencyInHertz, shortBeepInMilliseconds);
+                    Console.Write($"{shortBeepSymbol}");
+                }
+                Console.WriteLine();
+                Thread.Sleep(500);
+            } while (true);
+        }
+    }
+}
